810f17685390fb2e71aa45387eaae3b6bca335f4
ameybahugune <asd>
2025-04-25 13:31:38 +0530

    they are create a Ul desing a Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
-import './App.css'
-
-function App() {
-  
+import'./app.scss'
+function App() { 
 
   return (
     <main>
+ <section>
+  <div className="conversation-area">
 
+   <div className='input-area'>
+    <input type="text" placeholder='Meassage' />
+    <button>Send </button>
+    
+   </div>
+
+  </div>
+ </section>
     </main>
 
   )
